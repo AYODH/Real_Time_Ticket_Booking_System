7aa732dcbb2677ab01e2403f367f1792043af2c8
Rochani Riligala <137805971+AYODH@users.noreply.github.com>
2024-11-12 00:21:43 +0530

    System Initialization and Configuration

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+J
+src/RTTicketingSystem.java,6\8\68ad8da2a685c8db36e815eaeb30cf9fdf75eb3a
---
+++ src/RTTicketingSystem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RTTicketingSystem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        configureSystem(input);
+        startSystem(input);
+    }
+
+    private static void configureSystem(Scanner input) {
+        System.out.println("Welcome to Tickets.lk!");
+        System.out.println("Enter total number of tickets: ");
+        int totalTickets = input.nextInt();
+
+        System.out.println("Enter ticket release rate: ");
+        int ticketReleaseRate = input.nextInt();
+
+        System.out.println("Enter customer retrieval rate: ");
+        int customerRetrievalRate = input.nextInt();
+
+        System.out.println("Enter maximum ticket capacity: ");
+        int maxTicketCapacity = input.nextInt();
+    }
+
+    private static void startSystem(Scanner input) {
+    }
+}
